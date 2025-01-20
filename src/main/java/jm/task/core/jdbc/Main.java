c4fa709d2f059c1f93d7b93bafd11e821dae6815
Amina <ami2909@yandex.ru>
2025-01-20 23:16:54 +0300

    first try

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private static final UserService userService = new UserServiceImpl();
+    private static final User user1 = new User("Tomas", "Shelby", (byte) 30);
+    private static final User user2 = new User("Artur", "Shelby", (byte) 15);
+    private static final User user3 = new User("John", "Snow", (byte) 126);
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final Logger logger = LoggerFactory.getLogger(Util.class);
+    private static final String URL = "jdbc:mysql://localhost:3306/tab";
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            logger.info("Connection to DB succeeded!");
+        } catch (SQLException e) {
+            logger.error("Connection failed: {}", e.getMessage());
+        }
+        return connection;
+    }
 }
